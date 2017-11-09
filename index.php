554cbb6ad279b946c0b31cfd8401995e2370f11d
Joker-smile <1020733278@qq.com>
2017-11-09 10:52:09 +0800

    生成单张的png图片

--- index.php
+++ index.php
@@ -14,6 +14,10 @@ $file=file('test.txt');
 //print_r($files);
 for ($i=0;$i<count($file);$i++){
     $generator = new \Picqer\Barcode\BarcodeGeneratorPNG();
-    echo '<img  src="data:image/png;base64,' . base64_encode($generator->getBarcode(trim($file[$i]), $generator::TYPE_CODE_128)) . '" style="margin-top: 50px">'.'<br>'.'<span style="letter-spacing:10px;">'.trim($file[$i]).'</span>'.'<br>';
+//    echo '<img  src="data:image/png;base64,' . base64_encode($generator->getBarcode(trim($file[$i]), $generator::TYPE_CODE_128)) . '" style="margin-top: 50px">'.'<br>'.'<span style="letter-spacing:12px;">'.trim($file[$i]).'</span>'.'<br>';
+    //生成png图片并存放在img文件夹中
+    $content=$generator->getBarcode(trim($file[$i]), $generator::TYPE_CODE_128);
+    file_put_contents('img/'.trim($file[$i]).'.png',$content);
+    echo '<img  src="img/'.trim($file[$i]).'.png" style="margin-top: 50px">'.'<br>'.'<span style="letter-spacing:12px;">'.trim($file[$i]).'</span>'.'<br>';
 }
 
